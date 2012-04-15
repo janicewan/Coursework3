f842201c13159fcdecbce6c98d28eb1ccec28336
jwan01 <jwan01@dcs.bbk.ac.uk>
2012-04-15 14:56:15 +0100

    Initial Code

---
+++ Proxy/src/cd/Image.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package cd;
+
+import java.net.URL;
+
+/**
+ * @author jwan01
+ *
+ */
+public class Image{
+	URL imageURL;
+
+	public void showImage(URL url) {
+
+		/**
+		 * display image
+		 */
+		 System.out.println("HighRes Image");
+		
+	}
+}
